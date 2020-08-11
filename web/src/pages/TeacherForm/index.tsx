57ced6e0d30605c63ef212d72dfedc8b1e0be1b4
Gabriel Decilio <gdecilio@orion.tech>
2020-08-11 14:38:49 -0300

    available time

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -53,6 +53,32 @@ const TeacherForm: React.FC = () => {
           <Input name="whatsapp" label="WhatsApp" />
         </fieldset>
 
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">+ Novo horário</button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: "0", label: "Domingo" },
+                { value: "1", label: "Segunda-feira" },
+                { value: "2", label: "Terça-feira" },
+                { value: "3", label: "Quarta-feira" },
+                { value: "4", label: "Quinta-feira" },
+                { value: "5", label: "Sexta-feira" },
+                { value: "6", label: "Sabádo" },
+              ]}
+            />
+
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
+        </fieldset>
+
         <footer>
           <p>
             <img src={warningIcon} alt="Aviso important" />
